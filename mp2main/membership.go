19778c33917c02ae9e6977fd15479ce9fda2ba08
L-C-L <lclbangong@163.com>
2018-10-07 02:40:59 +0800

    Update membership.go

--- mp2main/membership.go
+++ mp2main/membership.go
@@ -531,7 +531,7 @@ func getGrepLog() {
 		IPList[i] = element.IpAddr
 	}
 
-	fmt.Println("Your input is :", strMessage)
+	//fmt.Println("Your input is :", strMessage)
 	mp1client.RunClientmain(strMessage, IPList)
 }
 
